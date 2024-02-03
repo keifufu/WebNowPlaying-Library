26111424d2ed390d64295b5c56539b1b29eea1d4
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-02-04 01:04:54 +0530

    Add Language bindings for python (hope it works, i havent tested it yet)

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+
+wnp = Extension(
+    "_wnp",
+    sources=["wnp_wrap.c", "wnp.c", "cws.c", "dp_windows.cpp"],
+    extra_compile_args=[
+        "/std:c++17",
+        "/D_CRT_SECURE_NO_WARNINGS",
+        "/link",
+        "/LIBPATH:C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1A\\Lib",
+        "Ws2_32.lib",
+    ],
+    libraries=['Ws2_32', 'gdiplus', 'advapi32', 'windowscodecs', 'ole32', 'oleaut32', 'uuid'],
+    define_macros=[
+        ('_WIN32_WINNT', '0x0600'),
+    ],
+)
+setup(
+    name="wnp",
+    version="0.1",
+    author="keifufu, YasogaN",
+    description="""WebNowPlaying is a browser extension that allows you to control music players from a browser popup. This is a Python wrapper for the WebNowPlaying-Library.""",
+    ext_modules=[wnp],
+    py_modules=["wnp"],
+)
