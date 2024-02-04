196b9eb136eb3cfe1f63e0fce9af32b65798dc0f
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-02-04 16:58:22 +0530

    Update Python build script to include package creation and cleanup

--- bindings/python/build.bat
+++ bindings/python/build.bat
@@ -7,4 +7,13 @@ swig -python -outdir . -o ../../src/wnp_wrap.c ../../src/wnp.i
 
 REM Run the build command
 ECHO Building the Python bindings as a python extension module
-python setup.py build
+python setup.py build_ext
+
+REM Create Packge
+ECHO Creating the Python package
+python setup.py sdist bdist_wheel
+
+REM Clean up
+ECHO Cleaning up
+del ..\src\* /q
+rd /s /q ..\src
