a4c93a26f50c413716761b985b0f38eb1198732b
keifufu <github@keifufu.dev>
2024-06-03 00:53:42 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "WebNowPlaying-Library";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs@{ self, flake-parts, ... }: 
+  flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    perSystem = { pkgs, system, ...}: {
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "libwnp";
+        version = builtins.readFile ./VERSION;
+        src = ./.;
+        buildInputs = with pkgs; [ clang makeWrapper ];
+        buildPhase = ''
+          make linux64
+        '';
+        installPhase = ''
+          mkdir -p $out/lib
+          mkdir -p $out/include
+          cp build/libwnp_linux_amd64.a $out/lib/libwnp.a
+          cp include/wnp.h $out/include/wnp.h
+        '';
+      };
+      devShells.default = pkgs.mkShell {
+        shellHook = "exec $SHELL";
+        buildInputs = with pkgs; [ clang valgrind gnumake ];
+      };
+    };
+  };
+}
+# TODO: header not built with packages.default
