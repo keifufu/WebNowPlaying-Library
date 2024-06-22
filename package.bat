04acb232f1318c9afcf487fe14f7df7022aaa5e7
keifufu <github@keifufu.dev>
2024-06-22 15:46:16 +0200

    update package scripts

--- package.bat
+++ package.bat
@@ -5,20 +5,20 @@ set /p VERSION=<VERSION
 
 set archive64=dist\libwnp-%VERSION%_win64_msvc.zip
 if exist %archive64% del %archive64%
-7z a %archive64% build\libwnp_win64.lib include\wnp.h CHANGELOG.md README.md LICENSE VERSION
+7z a %archive64% build\libwnp_win64.lib src\wnp.h CHANGELOG.md README.md LICENSE VERSION
 7z rn %archive64% build\libwnp_win64.lib lib/wnp.lib
 
 set archive64_nodp=dist\libwnp-%VERSION%_win64_msvc_nodp.zip
 if exist %archive64_nodp% del %archive64_nodp%
-7z a %archive64_nodp% build\libwnp_win64_nodp.lib include\wnp.h CHANGELOG.md README.md LICENSE VERSION
+7z a %archive64_nodp% build\libwnp_win64_nodp.lib src\wnp.h CHANGELOG.md README.md LICENSE VERSION
 7z rn %archive64_nodp% build\libwnp_win64_nodp.lib lib/wnp.lib
 
 set archive32=dist\libwnp-%VERSION%_win32_msvc.zip
 if exist %archive32% del %archive32%
-7z a %archive32% build\libwnp_win32.lib include\wnp.h CHANGELOG.md README.md LICENSE VERSION
+7z a %archive32% build\libwnp_win32.lib src\wnp.h CHANGELOG.md README.md LICENSE VERSION
 7z rn %archive32% build\libwnp_win32.lib lib/wnp.lib
 
 set archive32_nodp=dist\libwnp-%VERSION%_win32_msvc_nodp.zip
 if exist %archive32_nodp% del %archive32_nodp%
-7z a %archive32_nodp% build\libwnp_win32_nodp.lib include\wnp.h CHANGELOG.md README.md LICENSE VERSION
+7z a %archive32_nodp% build\libwnp_win32_nodp.lib src\wnp.h CHANGELOG.md README.md LICENSE VERSION
 7z rn %archive32_nodp% build\libwnp_win32_nodp.lib lib/wnp.lib
--- package.sh
+++ package.sh
@@ -5,8 +5,8 @@ version=$(<VERSION)
 
 archive="dist/libwnp-${version}_linux_amd64.tar.gz"
 rm -f $archive
-tar -cvzf $archive "build/libwnp_linux_amd64.a" --transform='s/build\/libwnp_linux_amd64.a/lib\/libwnp.a/' "include/wnp.h" "CHANGELOG.md" "README.md" "LICENSE" "VERSION" --show-transformed-names
+tar -cvzf $archive "build/libwnp_linux_amd64.a" --transform='s/build\/libwnp_linux_amd64.a/lib\/libwnp.a/' "src/wnp.h" "CHANGELOG.md" "README.md" "LICENSE" "VERSION" --show-transformed-names
 
 archive_nodp="dist/libwnp-${version}_linux_amd64_nodp.tar.gz"
 rm -f $archive_nodp
-tar -cvzf $archive_nodp "build/libwnp_linux_amd64_nodp.a" --transform='s/build\/libwnp_linux_amd64_nodp.a/lib\/libwnp.a/' "include/wnp.h" "CHANGELOG.md" "README.md" "LICENSE" "VERSION" --show-transformed-names
+tar -cvzf $archive_nodp "build/libwnp_linux_amd64_nodp.a" --transform='s/build\/libwnp_linux_amd64_nodp.a/lib\/libwnp.a/' "src/wnp.h" "CHANGELOG.md" "README.md" "LICENSE" "VERSION" --show-transformed-names
