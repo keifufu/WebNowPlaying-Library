72b604f3e4dacb9b931b34a8c93cce375b979acc
keifufu <github@keifufu.dev>
2024-10-06 18:57:39 +0200

    workflow

--- build.ps1
+++ build.ps1
@@ -14,43 +14,49 @@ function Invoke-Environment {
 }
 
 $origEnv = Get-ChildItem Env:*;
+$vsInstallationPath = & 'vswhere.exe' -property installationPath
 
-Remove-Item -Path "$PSScriptRoot\build" -Recurse -Force
-Remove-Item -Path "$PSScriptRoot\dist" -Recurse -Force
+if (-not $vsInstallationPath) {
+  Write-Host "No Visual Studio installation found."
+  exit 1
+}
+
+Remove-Item -Path "$PSScriptRoot\build" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$PSScriptRoot\dist" -Recurse -Force -ErrorAction SilentlyContinue
 
 New-Item -Path "$PSScriptRoot\build" -ItemType Directory
 New-Item -Path "$PSScriptRoot\build\win64" -ItemType Directory
 New-Item -Path "$PSScriptRoot\build\win32" -ItemType Directory
 New-Item -Path "$PSScriptRoot\dist" -ItemType Directory
 
-Invoke-Environment "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+Invoke-Environment "$vsInstallationPath\VC\Auxiliary\Build\vcvars32.bat"
 cmake -S "$PSScriptRoot" -B "$PSScriptRoot\build\win32" -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\build\win32" -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles"
 cmake --build "$PSScriptRoot\build\win32"
 cmake --install "$PSScriptRoot\build\win32"
 $origEnv | ForEach-Object { [System.Environment]::SetEnvironmentVariable($_.Name, $_.Value) }
 
-Invoke-Environment "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+Invoke-Environment "$vsInstallationPath\VC\Auxiliary\Build\vcvars64.bat"
 cmake -S "$PSScriptRoot" -B "$PSScriptRoot\build\win64" -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\build\win64" -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles"
 cmake --build "$PSScriptRoot\build\win64"
 cmake --install "$PSScriptRoot\build\win64"
 $origEnv | ForEach-Object { [System.Environment]::SetEnvironmentVariable($_.Name, $_.Value) }
 
 function package {
-    param (
-      [string]$platform
-    )
+  param (
+    [string]$platform
+  )
 
-    $files = @(
-      "$PSScriptRoot\README.md",
-      "$PSScriptRoot\LICENSE",
-      "$PSScriptRoot\CHANGELOG.md",
-      "$PSScriptRoot\VERSION",
-      "$PSScriptRoot\build\$platform\lib",
-      "$PSScriptRoot\build\$platform\include"
-    )
-
-    $version = Get-Content "$PSScriptRoot\VERSION" | Out-String
-    Compress-Archive -Path $files -DestinationPath "$PSScriptRoot\dist\libwnp-$version_$platform.zip" -CompressionLevel Optimal
+  $files = @(
+    "$PSScriptRoot\README.md",
+    "$PSScriptRoot\LICENSE",
+    "$PSScriptRoot\CHANGELOG.md",
+    "$PSScriptRoot\VERSION",
+    "$PSScriptRoot\build\$platform\lib",
+    "$PSScriptRoot\build\$platform\include"
+  )
+  
+  $version = (Get-Content "$PSScriptRoot\VERSION" | Out-String).Trim()
+  Compress-Archive -Path $files -DestinationPath "$PSScriptRoot\dist\libwnp-${version}_$platform.zip" -CompressionLevel Optimal
 }
 
 package -platform "win32"
